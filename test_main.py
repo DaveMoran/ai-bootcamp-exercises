614e977afb5b1a30080405330eca422e67e3a0b6
Dave Moran <dave.moran118@gmail.com>
2026-01-13 05:35:24 -0500

    create test case for shuffle

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from main import DeckOfCards, Card
@@ -14,3 +15,18 @@ class TestDeckOfCards(unittest.TestCase):
         assert isinstance(self.testDeck[0], Card)
         with self.assertRaises(IndexError):
             self.testDeck[52]
+
+    def test_shuffle(self):
+        beforeShuffleOne = self.testDeck[0]
+        beforeShuffleTwo = self.testDeck[1]
+        beforeShuffleThree = self.testDeck[2]
+
+        random.shuffle(self.testDeck)
+
+        afterShuffleOne = self.testDeck[0]
+        afterShuffleTwo = self.testDeck[1]
+        afterShuffleThree = self.testDeck[2]
+
+        assert beforeShuffleOne != afterShuffleOne
+        assert beforeShuffleTwo != afterShuffleTwo
+        assert beforeShuffleThree != afterShuffleThree
