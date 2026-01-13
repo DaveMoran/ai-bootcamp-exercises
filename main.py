40e18bc6fc27012a84e54b413df1b9fd34ac1461
Dave Moran <dave.moran118@gmail.com>
2026-01-13 05:22:11 -0500

    populate deck with cards

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ Card = collections.namedtuple("Card", ["rank", "suit"])
 
 
 class DeckOfCards:
+    ranks = [str(n) for n in range(2, 11) + list("JQKA")]
+    suits = "spades diamonds clubs hearts".split()
+
     def __init__(self):
-        deck = []
+        self.deck = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self.deck)
