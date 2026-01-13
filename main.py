2ca61fd594faa2935abdc63d4f18e30c7c278f9b
Dave Moran <dave.moran118@gmail.com>
2026-01-13 05:19:59 -0500

    add initial special methods

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ of its needs
 import collections
 
 Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class DeckOfCards:
+    def __init__(self):
+        deck = []
+
+    def __len__(self):
+        return len(self.deck)
+
+    def __getitem__(self, idx):
+        return self.deck[idx]
