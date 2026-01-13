ce1dc02782ec37487b8c049b0c2264015dfa7b6e
Dave Moran <dave.moran118@gmail.com>
2026-01-13 16:40:57 -0500

    Add skeleton tes cases

--- vector-2d/test_main.py
+++ vector-2d/test_main.py
@@ -7,3 +7,18 @@ from main import Vector2D
 class TestDeckOfCards(unittest.TestCase):
     def setUp(self):
         self.testVector = Vector2D(2, 3)
+
+    def testRepr(self):
+        pass
+
+    def testStr(self):
+        pass
+
+    def testAdd(self):
+        pass
+
+    def testMul(self):
+        pass
+
+    def testAbs(self):
+        pass
