fc80b283d724b707a1b433dda69e842201167d13
Dave Moran <dave.moran118@gmail.com>
2026-01-13 16:31:13 -0500

    Add rest of helper functions

--- vector-2d/main.py
+++ vector-2d/main.py
@@ -5,3 +5,15 @@ class Vector2D:
 
     def __repr__(self):
         return f"Vector2D(x={self.x}, y={self.y})"
+
+    def __str__(self):
+        return f"Vector2D Class. X:{self.x} Y:{self.y}"
+
+    def __add__(self):
+        pass
+
+    def __mul__(self):
+        pass
+
+    def __abs__(self):
+        pass
