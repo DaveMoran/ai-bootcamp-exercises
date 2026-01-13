db0c89e03de5af87f2b3a38324dd85091ab3c7b5
Dave Moran <dave.moran118@gmail.com>
2026-01-13 16:45:35 -0500

    add test for addition

--- vector-2d/test_main.py
+++ vector-2d/test_main.py
@@ -6,18 +6,22 @@ from main import Vector2D
 
 class TestDeckOfCards(unittest.TestCase):
     def setUp(self):
-        self.testVector = Vector2D(2, 3)
+        self.test_vector = Vector2D(2, 3)
 
     def test_repr(self):
-        repr_str = repr(self.testVector)
+        repr_str = repr(self.test_vector)
         assert repr_str == "Vector2D(x=2, y=3)"
 
     def test_str(self):
-        repr_str = str(self.testVector)
+        repr_str = str(self.test_vector)
         assert repr_str == "Vector2D | X:2, Y:3"
 
-    def testAdd(self):
-        pass
+    def test_add(self):
+        vector_to_add = Vector2D(5, 7)
+        added_vector = self.test_vector + vector_to_add
+
+        assert added_vector.x == 7
+        assert added_vector.y == 10
 
     def testMul(self):
         pass
