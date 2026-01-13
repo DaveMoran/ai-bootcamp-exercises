a755e73e355e55f67d260687b8aea1b2ce79a917
Dave Moran <dave.moran118@gmail.com>
2026-01-13 05:18:00 -0500

    start with initial card def

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ Exercise 1: Deck of Cards
 A simple Deck of Cards class that utlilizes special methods for most
 of its needs
 """
+
+import collections
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
