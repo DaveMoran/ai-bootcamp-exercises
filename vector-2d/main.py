8325c8c8d77adbf02ef675c6df6afce02b41ee8d
Dave Moran <dave.moran118@gmail.com>
2026-01-13 16:32:31 -0500

    refine add and multiply

--- vector-2d/main.py
+++ vector-2d/main.py
@@ -9,11 +9,11 @@ class Vector2D:
     def __str__(self):
         return f"Vector2D Class. X:{self.x} Y:{self.y}"
 
-    def __add__(self):
-        pass
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
 
-    def __mul__(self):
-        pass
+    def __mul__(self, scalar):
+        return Vector2D(self.x * scalar, self.y * scalar)
 
     def __abs__(self):
         pass
