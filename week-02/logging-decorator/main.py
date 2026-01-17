2905e30e49b9936e6e9146eb60b24087215bf781
Dave Moran <dave.moran118@gmail.com>
2026-01-17 10:50:24 -0500

    add vars and fn for logging_decorator

--- week-02/logging-decorator/main.py
+++ week-02/logging-decorator/main.py
@@ -1,9 +1,21 @@
 import functools
 import logging
 
+LOG_NAME = None
+LOG_MESSAGE = None
+
 
 class logging_decorator:
-    def __init__(self):
-        self.logname = ""
-        self.log = ""
-        self.logmsg = ""
+    def __init__(self, level, name=LOG_NAME, message=LOG_MESSAGE):
+        self.level = level
+        self.logname = name
+        self.log = logging.getLogger(self.logname)
+        self.logmsg = message
+
+    def __call__(self, fn):
+
+        def wrapper(*args, **kwargs):
+            self.log.log(self.level, self.logmsg)
+            return fn(*args, **kwargs)
+
+        return wrapper
