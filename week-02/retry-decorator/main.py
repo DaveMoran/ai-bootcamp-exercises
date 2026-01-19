01e29bd86d2c6db12b8f144a608e331b672fe8c9
Dave Moran <dave.moran118@gmail.com>
2026-01-18 23:41:38 -0500

    import from weekly homework

--- week-02/retry-decorator/main.py
+++ week-02/retry-decorator/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from retry-decorator!")
+import time
 
 
-if __name__ == "__main__":
-    main()
+class retry:
+    def __init__(self, max_attemps, delay):
+        """Setup the arguments to pass to our decorator"""
+        self.max_attempts = max_attempts
+        self.delay = delay
+
+    def __call__(self, fn):
+
+        def retrying(*_args):
+            """The retry function that will be run under the hood"""
+
+            counter = 0
+            while counter < max_attempts:
+                time.sleep(delay)
+                result = fn(*_args)
+                if custom_validation(result):
+                    return result
+
+            raise Exception(f"Function failed after {self.max_attempts} tries")
+
+        return retrying
+
+        def custom_validation(result):
+            """Custom validation logic placed here to exit out of while loop"""
