2132e5b55c28343db9ffb1e2dc815971fca8c1c0
Dave Moran <dave.moran118@gmail.com>
2026-01-19 00:02:39 -0500

    add test for custom exception

--- week-02/retry-decorator/test_main.py
+++ week-02/retry-decorator/test_main.py
@@ -12,3 +12,10 @@ def test_working_function_no_retry():
         result = divide(4, 2)
         assert result == 2
         mock_sleep.assert_not_called()
+
+
+def test_custom_exception(capsys):
+    divide(2, 0)
+    captured = capsys.readouterr()
+
+    assert "mathmatically impossible" in captured.out
