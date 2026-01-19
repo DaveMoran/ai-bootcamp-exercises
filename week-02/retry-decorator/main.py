494659ea848fc4904390d4f87a755aa17f4c6a45
Dave Moran <dave.moran118@gmail.com>
2026-01-18 23:43:41 -0500

    fix errors

--- week-02/retry-decorator/main.py
+++ week-02/retry-decorator/main.py
@@ -2,26 +2,26 @@ import time
 
 
 class retry:
-    def __init__(self, max_attemps, delay):
+    def __init__(self, max_attempts, delay):
         """Setup the arguments to pass to our decorator"""
         self.max_attempts = max_attempts
         self.delay = delay
 
     def __call__(self, fn):
 
+        def custom_validation(result):
+            """Custom validation logic placed here to exit out of while loop"""
+
         def retrying(*_args):
             """The retry function that will be run under the hood"""
 
             counter = 0
-            while counter < max_attempts:
-                time.sleep(delay)
+            while counter < self.max_attempts:
+                time.sleep(self.delay)
                 result = fn(*_args)
-                if custom_validation(result):
+                if result:
                     return result
 
             raise Exception(f"Function failed after {self.max_attempts} tries")
 
         return retrying
-
-        def custom_validation(result):
-            """Custom validation logic placed here to exit out of while loop"""
